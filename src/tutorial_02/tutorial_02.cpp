8c7d8f12e432c898762f561973836e5ddf08499d
idiasdas <igordias.ti@gmail.com>
2024-04-23 16:21:31 +0200

    [FEAT] Finished tutorial 02

---
+++ src/tutorial_02/tutorial_02.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+
+#include <GL/glew.h> // Always include glew before glfw
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+#include "common/shader.h"
+
+int main(int argc, char* argv[]){
+    // Initializing GLFW
+    if(!glfwInit()){
+        std::cout << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_SAMPLES, 4);                                // 4x antialiasing
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);                  // We want OpenGL 3.3
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // We don't want the old OpenGL
+
+    GLFWwindow* window;
+    window = glfwCreateWindow(1024, 768, "Tutorial 01", NULL, NULL);
+
+    if(window == nullptr){
+        std::cout << "Failed to open GLFW window" << std::endl;
+        std::cout << "If you have an Intel GPU, they are not 3.3 compatible." << std::endl;
+        std::cout << "Try the 2.1 version of the tutorials." << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // Initialize GLEW
+    const GLubyte* v = glewGetString(GLEW_VERSION);
+    std::cout << "GLEW version: " << v << std::endl;
+
+    glewExperimental = GL_TRUE;
+    if(glewInit() != GLEW_OK){
+        std::cout << "Failed to initialize GLEW" << std::endl;
+        return -1;
+    }
+
+    // Ensure we can capture the escape key being pressed below
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+    // Tutorial 02 --------------------------------------------------------------------------------------
+    // Defining the triangle vertices
+    GLuint VertexArrayID;
+    glGenVertexArrays(1, &VertexArrayID);
+    glBindVertexArray(VertexArrayID);
+
+    static const GLfloat g_vertex_buffer_data[] = {
+        -1.0f, -1.0f, 0.0f,
+        1.0f, -1.0f, 0.0f,
+        0.0f,  1.0f, 0.0f,
+    };
+
+    // Giving the triangle vertices to OpenGL
+    GLuint vertexbuffer;
+    glGenBuffers(1, &vertexbuffer);// Now vertexbuffer contains the buffer ID
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+    GLuint programID = LoadShaders("../src/tutorial_02/SimpleVertexShader.vertexshader", "../src/tutorial_02/SimpleFragmentShader.fragmentshader");
+
+    glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+    // Tutorial 02 --------------------------------------------------------------------------------------
+
+    do{
+        // Tutorial 02 ----------------------------------------------------------------------------------
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // 1st attribute buffer : vertices
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        glVertexAttribPointer(
+            0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+            3,                  // size
+            GL_FLOAT,           // type
+            GL_FALSE,           // normalized?
+            0,                  // stride
+            (void*)0            // array buffer offset
+        );
+
+        // Use our shader
+        glUseProgram(programID);
+
+        // Draw the triangle !
+        glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
+
+        glDisableVertexAttribArray(0);
+        // Tutorial 02 ----------------------------------------------------------------------------------
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    } while(glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0);
+
+    return 0;
+}
